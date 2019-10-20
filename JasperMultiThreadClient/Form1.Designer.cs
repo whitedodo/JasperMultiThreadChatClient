2f7269abb6412e30edf03b57416474b6dc26fd61
Dodo(NickName) <rabbit.white@daum.net>
2019-10-20 20:54:05 +0900

    Signed-off-by: Dodo(NickName) <rabbit.white@daum.net>

---
+++ JasperMultiThreadClient/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace JasperMultiThreadClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.lblServerStatus = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblNickname = new System.Windows.Forms.Label();
+            this.txtNickname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.BackColor = System.Drawing.SystemColors.Window;
+            this.txtMessages.Location = new System.Drawing.Point(12, 80);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ReadOnly = true;
+            this.txtMessages.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessages.Size = new System.Drawing.Size(557, 223);
+            this.txtMessages.TabIndex = 2;
+            // 
+            // lblServerStatus
+            // 
+            this.lblServerStatus.AutoSize = true;
+            this.lblServerStatus.Location = new System.Drawing.Point(13, 55);
+            this.lblServerStatus.Name = "lblServerStatus";
+            this.lblServerStatus.Size = new System.Drawing.Size(38, 12);
+            this.lblServerStatus.TabIndex = 2;
+            this.lblServerStatus.Text = "label1";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 322);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(459, 21);
+            this.txtSend.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(477, 321);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(91, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "보내기(Send)";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // lblNickname
+            // 
+            this.lblNickname.AutoSize = true;
+            this.lblNickname.Location = new System.Drawing.Point(375, 54);
+            this.lblNickname.Name = "lblNickname";
+            this.lblNickname.Size = new System.Drawing.Size(96, 12);
+            this.lblNickname.TabIndex = 5;
+            this.lblNickname.Text = "별명(Nickname)";
+            // 
+            // txtNickname
+            // 
+            this.txtNickname.Location = new System.Drawing.Point(475, 50);
+            this.txtNickname.Name = "txtNickname";
+            this.txtNickname.Size = new System.Drawing.Size(94, 21);
+            this.txtNickname.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 354);
+            this.Controls.Add(this.txtNickname);
+            this.Controls.Add(this.lblNickname);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lblServerStatus);
+            this.Controls.Add(this.txtMessages);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtMessages;
+        private System.Windows.Forms.Label lblServerStatus;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer refreshTimer;
+        private System.Windows.Forms.Label lblNickname;
+        private System.Windows.Forms.TextBox txtNickname;
+    }
+}
+
---
+++ JasperMultiThreadClient/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JasperMultiThreadClient
+{
+    public partial class Form1 : Form
+    {
+        Thread thTimerThread;
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream serverStream;
+
+        string address;
+        int port;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            address = "127.0.0.1";
+            port = 8888;
+            lblServerStatus.Text = DateTime.Now + " 환영합니다.(Welcome)";
+
+            thTimerThread = new Thread(serverTimerStart);
+            thTimerThread.Start();
+        }
+
+        public void serverTimerStart()
+        {
+
+            while (true)
+            {
+                refreshTimer.Interval = 3000;
+                refreshTimer.Enabled = true;
+                refreshTimer.Start();
+            }
+
+        }
+
+        private void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket.Connect(address, port);
+                lblServerStatus.Text = "서버 작동중...(Server On...)";
+                recvMessage();
+
+            }
+            catch (Exception ex)
+            {
+                lblServerStatus.Text = "서버 멈췄음(Server Stopped)";
+                txtMessages.Text = txtMessages.Text + Environment.NewLine +
+                                   ex.ToString();
+            }
+        }
+
+        public void recvMessage()
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] inStream = new byte[65536];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+
+            string returnData = System.Text.Encoding.UTF8.GetString(inStream);
+            txtMessages.Text = txtMessages.Text + Environment.NewLine + ">>" + returnData;
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            sendMessage();
+        }
+
+        public void sendMessage()
+        {
+            string localIP = GetLocalIP();
+            string usrNickname = txtNickname.Text;
+            string usrMessage = txtSend.Text;
+
+            // 닉네임
+            if (usrNickname == "")
+            {
+                usrNickname = "익명(Anonymous)";
+            }
+
+            // 메시지(message)
+            if ( usrMessage == "")
+            {
+                usrMessage = localIP + ";" + usrNickname + ";\0";
+            }
+            else
+            {
+                usrMessage = localIP + ";" + usrNickname + ";" + usrMessage;
+            }
+
+            // 연결 여부(Connection)
+            if (clientSocket.Connected == true)
+            {
+                serverStream = clientSocket.GetStream();
+                byte[] outStream = System.Text.Encoding.UTF8.GetBytes(usrMessage);
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+            }
+            else
+            {
+                try
+                {
+                    clientSocket.Connect(address, port);
+                    lblServerStatus.Text = "서버 작동중...(Server On...)";
+                    serverStream = clientSocket.GetStream();
+                    byte[] outStream = System.Text.Encoding.UTF8.GetBytes(usrMessage);
+                    serverStream.Write(outStream, 0, outStream.Length);
+                    serverStream.Flush();
+                    recvMessage();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("대상 컴퓨터에서 연결을 거부했으므로 연결하지 못했습니다.\n" + 
+                                    "(Failed to connect because the destination computer refused to connect.)",
+                                    "메시지(Message)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    lblServerStatus.Text = "오류(Error)";
+                    txtMessages.Text = txtMessages + Environment.NewLine +
+                                       ex.ToString();
+                }
+            }
+        }
+
+        public string GetLocalIP()
+        {
+            string localIP = "Not available, please check your network seetings!";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thTimerThread.Abort();
+        }
+    }
+}
